3489c67642399d1963508871f05bc41a326559de
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2019-04-10 13:16:55 +0100

    webextension: set up icon to be rendered to multiple sizes of png at build time

--- default.nix
+++ default.nix
@@ -1,22 +1,29 @@
 {
   pkgs ? import <nixpkgs> {}
 }:
+
 let
+  renderedIcon = svgIcon: size: pkgs.runCommand "out-${toString size}.png" {
+    buildInputs = [ pkgs.inkscape ];
+  } ''
+    inkscape -z -C -h ${toString size} -f ${svgIcon} -e $out
+  '';
+in rec {
   stagingDir = pkgs.linkFarm "digitalmarketplace-logdia-stagingdir" ((map (s: { name = s; path = ./. + ("/" + s); }) [
     "default.nix"  # include self in archive for transparency purposes
     "background.js"
-    "icon48.svg"
     "index.html"
     "LICENCE"
     "README.md"
     "logdia.js"
     "manifest.json"
     "xhr-monkeypatcher.js"
-    # the repo-bundled copy of d3 is really just there to allow github pages to work and to ease development - by
-    # default we omit it here
-    # "d3.v4.js" 
+    # the repo-bundled copies of d3 & browser-polyfill are really just there to allow github pages to work and/or to
+    # ease development - by default we omit them here:
+    # "d3.v4.js"
+    # "browser-polyfill.js"
   ]) ++ [
-    # ...instead using a reproducibly-sourced, minified copy of d3
+    # ...instead using reproducibly-sourced, minified copies of them
     {
       name = "d3.v4.js";
       path = pkgs.runCommand "d3.v4.min.js" {
@@ -30,8 +37,17 @@ let
         cp d3.min.js $out
       '';
     }
-  ]);
-in {
+    {
+      name = "browser-polyfill.js";
+      path = pkgs.fetchurl {
+        url = "https://unpkg.com/webextension-polyfill@0.4.0/dist/browser-polyfill.min.js";
+        sha256 = "0d97xa5cnkzhlsv3gmm5a4kgqsfgglhspx9kv2ccmb22gqipywfc";
+      };
+    }
+  ] ++ (map (size: {  # we also render multiple resolutions of icon as png because chrome can't handle svg icons
+    name = "icons/digitalmarketplace-logdia-${toString size}.png";
+    path = renderedIcon ./icons/digitalmarketplace-logdia-48.svg size;
+  }) [16 19 24 38 48]));
   digitalmarketplace-logdia-xpi = pkgs.runCommand "digitalmarketplace-logdia.xpi" {
     buildInputs = [ pkgs.zip ];
   } ''
